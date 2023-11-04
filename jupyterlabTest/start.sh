48fe88505ecc94b1f2114b279eb6d2c60317ffb0
xngh <87764948+xngh@users.noreply.github.com>
2023-11-04 10:19:01 +0800

    Update start.sh

--- jupyterlabTest/start.sh
+++ jupyterlabTest/start.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+cd ~
+mkdir jupyter_workspace
+
+# to do, wget the zip file and unzip the contents to jupyter workspace
+# we use the following 2 rows as example instead
+touch main.py
+mv main.py jupyter_workspace/
+
+jupyter lab --notebook-dir jupyter_workspace --ip 0.0.0.0 --port 8888 --allow-root --no-browser --ServerApp.password="" --ServerApp.token=""
 
