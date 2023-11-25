57b622f0d87d8513d132be092f222ec4df51a650
xngh <87764948+xngh@users.noreply.github.com>
2023-11-25 10:25:04 +0800

    Update start.sh：需要使用交互式终端设置配置文件，因此更改了脚本

--- jupyterlabTest/start.sh
+++ jupyterlabTest/start.sh
@@ -1,11 +1,24 @@
-#!/bin/bash
-cd ~
-mkdir jupyter_workspace
+apt-get update
+apt-get install expect
+apt-get install jq
+jupyter lab --generate-config 
 
-# to do, wget the zip file and unzip the contents to jupyter workspace
-# we use the following 2 rows as example instead
-touch main.py
-mv main.py jupyter_workspace/
+#使用expect处理交互式命令行程序
+expect -c "
+spawn jupyter lab password
+expect \"Enter password:\"
+send \"$USER_PASSWORD\r\"
+expect \"Verify password:\"
+send \"$USER_PASSWORD\r\"
+expect eof
+"
+sleep 0.5
+# Copy hashed password
+hashed_password=$(jq -r '.IdentityProvider.hashed_password' ~/.jupyter/jupyter_server_config.json)
+# echo $hashed_password
 
-jupyter lab --notebook-dir jupyter_workspace --ip 0.0.0.0 --port 8888 --allow-root --no-browser --ServerApp.password="ndc@wwg1234" --ServerApp.token=""
+# Update Jupyter Lab config file with the hashed password
+sed -i "s|^# c.ServerApp.password = .*|c.ServerApp.password = '$hashed_password'|" ~/.jupyter/jupyter_lab_config.py
 
+# Run Jupyter Lab
+jupyter lab --notebook-dir jupyter_workspace --ip 0.0.0.0 --port 8888 --allow-root --no-browser 
